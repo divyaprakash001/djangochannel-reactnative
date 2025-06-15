587c27d7152e63e30bc211cf23bc8adf2594458f
Divya Prakash <divyaprakashara09@gmail.com>
2025-06-15 14:49:50 +0530

    Fix AppRegistryNotReady by calling django.setup() in asgi.py

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -1,49 +1,22 @@
-
-# import chat.routing
-# import os
-
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from channels.security.websocket import AllowedHostsOriginValidator
-# from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
-# from django.core.asgi import get_asgi_application
-
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-# django_asgi_app = get_asgi_application()
-
-# application = ProtocolTypeRouter({
-#   'http': django_asgi_app,
-#   'websocket':AllowedHostsOriginValidator(
-#     JWTAuthMiddlewareStack(
-#       URLRouter(chat.routing.websocket_urlpatterns)
-#     )
-#   )
-# })
-
-
 import os
-import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# Setup Django
-django.setup()
-
-# Now safe to import routing and get_asgi_application
-import chat.routing
-
+# 👇 Call this first to initialize Django apps
 django_asgi_app = get_asgi_application()
 
+# 👇 Now import things that rely on Django being set up
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
+import chat.routing
+
 application = ProtocolTypeRouter({
-    'http': django_asgi_app,
-    'websocket': AllowedHostsOriginValidator(
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
         JWTAuthMiddlewareStack(
             URLRouter(chat.routing.websocket_urlpatterns)
         )
-    )
+    ),
 })
