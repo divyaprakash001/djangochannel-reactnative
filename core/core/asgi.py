fc3b281b5f1c28e80e64e48e4ff891f55bc175c3
Divya Prakash <divyaprakashara09@gmail.com>
2025-06-15 14:43:29 +0530

    Fix AppRegistryNotReady by calling django.setup() in asgi.py

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -1,30 +1,49 @@
-"""
-ASGI config for core project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+# import chat.routing
+# import os
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.security.websocket import AllowedHostsOriginValidator
+# from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
+# from django.core.asgi import get_asgi_application
 
-import chat.routing
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# django_asgi_app = get_asgi_application()
+
+# application = ProtocolTypeRouter({
+#   'http': django_asgi_app,
+#   'websocket':AllowedHostsOriginValidator(
+#     JWTAuthMiddlewareStack(
+#       URLRouter(chat.routing.websocket_urlpatterns)
+#     )
+#   )
+# })
+
+
 import os
-
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-
+from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Setup Django
+django.setup()
+
+# Now safe to import routing and get_asgi_application
+import chat.routing
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-  'http': django_asgi_app,
-  'websocket':AllowedHostsOriginValidator(
-    JWTAuthMiddlewareStack(
-      URLRouter(chat.routing.websocket_urlpatterns)
+    'http': django_asgi_app,
+    'websocket': AllowedHostsOriginValidator(
+        JWTAuthMiddlewareStack(
+            URLRouter(chat.routing.websocket_urlpatterns)
+        )
     )
-  )
 })
